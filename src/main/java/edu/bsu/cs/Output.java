710dd8c3a836623bc73b251a0514e6e556c084f9
Black-Aichi <tyler.stout@bsu.edu>
2025-09-22 21:05:45 -0400

    renaming of ViewerTest to WikipediaTest. renaming of SorterTest to RevisionTest

--- src/main/java/edu/bsu/cs/Output.java
+++ src/main/java/edu/bsu/cs/Output.java
@@ -1,4 +1,16 @@
 package edu.bsu.cs;
 
+import java.util.Scanner;
+
 public class Output {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String userSearchPrompt() {
+        System.out.println("Enter your search: ");
+        return scanner.nextLine();
+    }
+
+    public void printRawJson (String jsonData){
+        System.out.println(jsonData);
+    }
 }
--- src/main/java/edu/bsu/cs/Viewer.java
+++ src/main/java/edu/bsu/cs/Viewer.java
@@ -1,4 +1,16 @@
 package edu.bsu.cs;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URLConnection;
+
+import static edu.bsu.cs.APIFunctionality.connectToWikipedia;
+import static edu.bsu.cs.APIFunctionality.readJsonAsStringFrom;
+
 public class Viewer {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        URLConnection connection = connectToWikipedia();
+        String jsonData = readJsonAsStringFrom(connection);
+        printRawJson(jsonData);
+    }
 }

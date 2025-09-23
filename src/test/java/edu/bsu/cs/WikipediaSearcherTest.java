a8911ada3aa3c81224ebb6aa0bb1f55e4c8a2e1c
Black-Aichi <tyler.stout@bsu.edu>
2025-09-23 16:04:09 -0400

    Transferred code from WikipediaSearcherTest to WikipediaSearcher

--- src/test/java/edu/bsu/cs/WikipediaSearcherTest.java
+++ src/test/java/edu/bsu/cs/WikipediaSearcherTest.java
@@ -1,35 +1,4 @@
 package edu.bsu.cs;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-import java.nio.charset.Charset;
-
 public class WikipediaSearcherTest {
-
-    public String getPageRevisions(String title, int limit) {
-        if (limit < 1 || limit > 16) {
-            throw new ErrorsTest.BadRequest("Change limit must be between 1 and 15.");
-        }
-        try {
-            String base = "https://en.wikipedia.org/w/api.php";
-            String query = "action=query" + "&format=json"+"&formatversion=2"+"&prop=revisions"+"&titles=" +
-                    URLEncoder.encode(title, Charset.defaultCharset()) + "&rvprop=timestamp|user"+"&rvlimit=" +
-                    limit + "&redirects=1";
-
-            URI uri = new URI(base + "?"+ query);
-            URLConnection connection = uri.toURL().openConnection();
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(5000);
-            connection.connect();
-
-            byte [] bytes = connection.getInputStream().readAllBytes();
-            return new String(bytes, Charset.defaultCharset());
-        }
-        catch (IOException | URISyntaxException error) {
-            throw new ErrorsTest.Network(error);
-        }
-    }
 }

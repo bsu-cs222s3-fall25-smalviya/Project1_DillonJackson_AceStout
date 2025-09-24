4d6cc738b5afede52e8fcc293054d4bdbf9320f9
Black-Aichi <tyler.stout@bsu.edu>
2025-09-23 21:11:14 -0400

    Correct WikipediaSearcher

--- src/main/java/edu/bsu/cs/WikipediaSearcher.java
+++ src/main/java/edu/bsu/cs/WikipediaSearcher.java
@@ -10,17 +10,23 @@ import java.nio.charset.Charset;
 public class WikipediaSearcher {
 
     public String getPageRevisions(String title, int limit) {
-        if (limit < 1 || limit > 16) {
+        if (limit < 1 || limit > 50){
             throw new Errors.BadRequest("Change limit must be between 1 and 15.");
         }
         try {
             String base = "https://en.wikipedia.org/w/api.php";
-            String query = "action=query" + "&format=json"+"&formatversion=2"+"&prop=revisions"+"&titles=" +
-                    URLEncoder.encode(title, Charset.defaultCharset()) + "&rvprop=timestamp|user"+"&rvlimit=" +
-                    limit + "&redirects=1";
+            String query = "action=query"
+                    + "&format=json"
+                    +"&formatversion=2"
+                    +"&prop=revisions"
+                    +"&titles=" + URLEncoder.encode(title, Charset.defaultCharset())
+                    + "&rvprop=timestamp%7Cuser"
+                    +"&rvlimit=" + limit
+                    + "&redirects=1";
 
             URI uri = new URI(base + "?"+ query);
             URLConnection connection = uri.toURL().openConnection();
+            connection.setRequestProperty("User-Agent", "BSU-CS-WikiSearch/1.0 (student project)");
             connection.setConnectTimeout(5000);
             connection.setReadTimeout(5000);
             connection.connect();
